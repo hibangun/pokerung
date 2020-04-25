30ce7e65162903ea5d0da3dffb7c68b47c3b3d43
Bangun <hibangun@gmail.com>
2020-04-25 12:01:15 +0700

    Get similar pokemons

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -1,46 +1,120 @@
 import { h, Component } from 'preact';
 import style from './style';
 
+import Pokemon from '../../components/pokemon'
+
 export default class Profile extends Component {
   state = {
     time: Date.now(),
-    count: 10
-  };
-
-  // update the current time
-  updateTime = () => {
-    this.setState({ time: Date.now() });
-  };
-
-  increment = () => {
-    this.setState({ count: this.state.count+1 });
+    count: 10,
+    profile: {},
+    profileTypes: [],
+    similars: []
   };
 
   // gets called when this route is navigated to
   componentDidMount() {
-    // start a timer for the clock:
-    this.timer = setInterval(this.updateTime, 1000);
+    this.getPokemonProfile()
+  }
+
+  componentWillReceiveProps (nextProps) {
+    this.getPokemonProfile(nextProps.name)
+    window.scrollTo(0, 0)
+  }
+
+  componentWillMount () {
+    if (!Object.keys(this.state.profile).length) {
+      this.getPokemonProfile()
+    }
+  }
+
+  async getPokemonProfile (name = null) {
+    const pokemonName = name || this.props.name
+
+    const pokemonId = pokemonName.split('•--')[1]
+
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+    let data = await response.json()
+
+    let profileTypes = []
+    data.types && data.types.map(item => {
+      return profileTypes.push(item.type.name)
+    })
+
+    this.setState({
+      profile: data,
+      profileTypes
+    })
+
+    this.getSimilarPokemon(profileTypes)
+  }
+
+  async getSimilarPokemon (arr) {
+    arr.map(async (item) => {
+      let response = await fetch(`https://pokeapi.co/api/v2/type/${item}`)
+      let data = await response.json()
+
+      const joined = this.state.similars.concat(data)
+      this.setState({
+        similars: joined
+      })
+    })
   }
 
   // gets called just before navigating away from the route
-  componentWillUnmount() {
-    clearInterval(this.timer);
+  componentWillUnmount () {
+    this.setState({
+      profile: {}
+    })
   }
 
-  // Note: `user` comes from the URL, courtesy of our router
-  render({ user }, { time, count }) {
+  capitalize (str) {
+    return str && str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+  }
+
+  // Note: `name` comes from the URL, courtesy of our router
+  render ({ name }, { profile, similars }) {
+    const id = name.split('•--')[1]
+
     return (
       <div class={style.profile}>
-        <h1>Profile: {user}</h1>
-        <p>This is the user profile for a user named { user }.</p>
-
-        <div>Current time: {new Date(time).toLocaleString()}</div>
-
-        <p>
-          <button onClick={this.increment}>Click Me</button>
-          {' '}
-          Clicked {count} times.
-        </p>
+        <div class={style.container}>
+          <div class={style.header}>
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt="" />
+          </div>
+          <div class={style.card}>
+            <h1>{this.capitalize(profile.name)}</h1>
+            <div class={style.summary}>
+              <div class={style.type}>
+                <p>
+                  {profile.types && profile.types.map((item, i) => {
+                    return <span>{item.type.name}</span>
+                  })}
+                </p>
+                <small>Type</small>
+              </div>
+              <div class={style.weight}>
+                <p>{profile.weight}</p>
+                <small>weight</small>
+              </div>
+              <div class={style.height}>
+                <p>{profile.height}</p>
+                <small>height</small>
+              </div>
+            </div>
+          </div>
+          <div class={style.similars}>
+            <h2>Similar</h2>
+            {similars.length && similars[0].pokemon.map(item => {
+              // console.log('Profile -> render -> item', item);
+              const n = item.pokemon.url.replace('https://pokeapi.co/api/v2/pokemon/', '').replace('/', '')
+              item.pokemon.n = n
+              item.pokemon.image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${n}.png`
+              // return <span>{item.pokemon}</span>
+              return <Pokemon data={item.pokemon} />
+            })}
+          </div>
+        </div>
       </div>
     );
   }
