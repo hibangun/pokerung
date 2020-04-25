589d43f44d042df683954ca94404fa0b7d6a1c92
Bangun <hibangun@gmail.com>
2020-04-25 22:43:59 +0700

    Set header as link

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,13 @@
+import { Link } from 'preact-router/match'
 import style from './style.css';
 
 const Header = () => (
   <header class={style.header}>
     <div class={style.container}>
-      <div class={style.logo}>
-        <img src="/assets/images/logo.png" alt="Pokerung Logo"/>
+      <Link class={style.logo} href="/">
+        <img src="/assets/images/logo.png" alt="Pokerung Logo" />
         <h1>Pokerung</h1>
-      </div>
+      </Link>
     </div>
   </header>
 );
